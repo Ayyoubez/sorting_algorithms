26effbe0a3275d7d0602a12e89ff75fedc7604d3
Ayyoub <ayyoub@DEV.myguest.virtualbox.org>
2024-02-15 00:53:37 +0100

    next task update

---
+++ 0-bubble_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sort an array elements
+ * @array: array
+ * @size: size of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, idx, temp =0;
+
+	if (size < 2)
+		return;
+	if (i = 0; i < size: i++)
+		for (idx = 0; idx < size; idx++)
+		{
+			if (array[idx] > array[idx + 1] && array[idx + 1])
+			{
+				temp = array[idx];
+				array[idx] = array[idx + 1];
+				array[idx + 1] = temp;
+				print_array(array, size);
+			}
+		}
+}
