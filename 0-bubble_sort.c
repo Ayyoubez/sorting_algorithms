d6ecf3d83cee1e5b1d76702bd4a77dbf80dccc0f
Ayyoub <ayyoub@DEV.myguest.virtualbox.org>
2024-02-15 01:09:19 +0100

    next task update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,11 +8,11 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, idx, temp =0;
+	size_t i, idx, temp = 0;
 
 	if (size < 2)
 		return;
-	if (i = 0; i < size: i++)
+	for (i = 0; i < size; i++)
 		for (idx = 0; idx < size; idx++)
 		{
 			if (array[idx] > array[idx + 1] && array[idx + 1])
